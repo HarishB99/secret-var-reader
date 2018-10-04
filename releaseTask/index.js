b66ab3a71323941cd7c620c3ccaa598f09f8a8e8
Harish <harish_balamurugan@yahoo.com.sg>
2018-10-04 13:26:37 +0800

    Read environment variables using task.getVariable instead of process.env

--- releaseTask/index.js
+++ releaseTask/index.js
@@ -9,7 +9,15 @@ try {
         const password = task.getInput('password', true);
         console.log(`[i] password: ${password}`);
     console.log();
-    console.log('[+] Storing input variables: Complete');
+    console.log('[+] Storing secret: Complete');
+    console.log();
+    console.log();
+    console.log('[i] Retrieving environment variables: Started');
+    console.log();
+        const email = task.getVariable('Release.RequestedForEmail');
+        console.log(`[i] email: ${email}`);
+    console.log();
+    console.log('[+] Retrieving environment variables: Complete');
     console.log();
 } catch (error) {
     console.log();
